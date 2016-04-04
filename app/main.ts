ce795e60b03285da71e894cdd05d6cb09ad1cba2
Jack Neary <xeeeveee@gmail.com>
2016-04-04 23:45:07 +0100

    Include the todo service in the bootstrap

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './components/app/app.component';
+import {TodoService} from './services/todo-service';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [TodoService]);
