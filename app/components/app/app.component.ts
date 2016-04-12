c61d601480cf1ad5f0d45091fdb7fb98fc22b9b0
Jack Neary <xeeeveee@gmail.com>
2016-04-12 22:25:10 +0100

    Add todo status element to application
    Ideally want to move this into the list component it's self

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';
 import {Toolbar} from '../toolbar/toolbar.component'
 import {TodoListComponent} from '../todo-list/todo-list.component'
+import {TodoStatusComponent} from "../todo-status/todo-status.component";
 
 @Component({
     selector: 'app',
-    template: '<toolbar></toolbar><div class="container"><todo-list></todo-list></div>',
-    directives: [TodoListComponent, Toolbar]
+    template: '<toolbar></toolbar><div class="container"><todo-status (update)="status = $event"></todo-status><todo-list [status]="status"></todo-list></div>',
+    directives: [TodoListComponent, TodoStatusComponent, Toolbar]
 })
 
 export class AppComponent {
